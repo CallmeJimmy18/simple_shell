9cffc2ad492d31487c46544e1d447da817bcc627
Liam Bowers <liambowers718@gmail.com>
2023-07-26 00:01:43 -0700

    Removed betty errors

--- shell.h
+++ shell.h
@@ -16,9 +16,9 @@ void tokeniz(char *cmd, char **args);
 int _strcmp(char *s1, char *s2);
 int str_cmp(char *s1, char *s2);
 char *enpath(char **list);
-int copy(char *dest, char*src);
-int add(char *dest, char*src);
+int copy(char *dest, char *src);
+int add(char *dest, char *src);
 char *get_path(char *dir, char **env);
-void print_env(char ** env);
+void print_env(char **env);
 void prints(char *s);
 #endif
